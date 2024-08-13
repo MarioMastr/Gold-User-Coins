35d8ec47a7ba2c4b202c1ec266df69a99e3b4c07
MarioMastr <ashrmoha11@gmail.com>
2024-08-13 20:17:10 +0100

    patches begone!!!

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # GoldUserCoins
 
-Geode mod that causes all in-game user coins to be rendered as gold secret coins. 100% cosmetic
+The epic texture swapping mod by the best smelling cologne in the GD community himself... now **without patches**!
+
+This was mainly done as an excuse to learn some raw reverse engineering (thank you to whoever leaked IDA 9.0 for your immense hard work 🫡), but also to make this mod truly multi-platform (maybe, I haven't tested this for Android myself but it *should* work).
+This mod does use some bindings that are currently unavailable for m1 and mainly imac atm, but here are the bindings for them both that you can put in your own bindings repo if you wish:
+```
+void EnhancedGameObject::updateUserCoin() = win 0x19c930, m1 0x4f958c, imac 0x5ca6b0;
+void GameObject::playPickupAnimation(cocos2d::CCSprite*, float, float, float, float) = win 0x19ff10, m1 0x20184c, imac 0x259e00;
+void GameObject::playDestroyObjectAnim(GJBaseGameLayer*) = win 0x1a04e0, m1 0x201d4c, imac 0x25a310;
+cocos2d::CCParticleSystemQuad*  GJBaseGameLayer::spawnParticle(char const*, int, cocos2d::tCCPositionType, cocos2d::CCPoint) = win 0x22f3f0, m1 0x1222ec, imac 0x151480;
+```
+Cocos bindings are needed as well (yes, for CCAnimate & CCAnimation you *need* to put in the entire thing in this manner otherwise they won't link properly for m1 and imac):
+```
+[[link(win, android)]]
+class cocos2d::CCAnimation {
+	static cocos2d::CCAnimation *createWithSpriteFrames(CCArray *, float) = m1 0x235a88, imac 0x2927e0;
+}
+
+[[link(win, android)]]
+class cocos2d::CCAnimate {
+	static cocos2d::CCAnimate *create(cocos2d::CCAnimation*) = m1 0x342ea0, imac 0x3bf2f0;
+}
+
+void cocos2d::CCSpriteFrameCache::removeSpriteFrameByName(char const*) = m1 0x2acbec, imac 0x31bda0;
+void cocos2d::CCSpriteFrameCache::addSpriteFrame(cocos2d::CCSpriteFrame*, char const*) = m1 0x2aca14, imac 0x30e2e0;
+```
+
+This does result in a little bit of a conflict with QOLMod (but honestly everything does so don't worry too much).
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/LoadingLayer.hpp>
 #include <Geode/modify/CCSprite.hpp>
+#include <Geode/modify/EnhancedGameObject.hpp>
+#include <Geode/modify/GameObject.hpp>
 
 using namespace geode::prelude;
 
@@ -49,14 +51,220 @@ class $modify(LoadingLayer) {
 	}
 };
 
+class $modify(EnhancedGameObject) {
+    static void onModify(auto& self) {
+        self.setHookPriority("EnhancedGameObject::updateUserCoin", 1024);
+    }
+
+	void updateUserCoin() {
+		// time for a complete reverse engineer!!!
+
+  		bool hasUniqueCoin; // w0
+  		cocos2d::CCParticleSystemQuad *v4; // x0
+  		cocos2d::CCParticleSystemQuad *v5; // x20
+  		int v6; // s0
+  		int v12; // s0
+  		__int64_t v13; // [xsp+0h] [xbp-20h] BYREF (its __int64_t and not __int64 because __int64 doesnt exist on mac :>)
+  		int v14; // [xsp+8h] [xbp-18h]
+  		int v15; // [xsp+Ch] [xbp-14h]
+
+		if (!this->m_editorEnabled) {
+			hasUniqueCoin = GJBaseGameLayer::get()->hasUniqueCoin((EffectGameObject *)this);
+			this->m_obAnchorPointInPoints.x = hasUniqueCoin;
+			if (!hasUniqueCoin) {
+				v4 = this->createAndAddParticle((int)this->m_objectType, "coinEffect.plist", 0, kCCPositionTypeGrouped);
+				if (v4) {
+					if (this->m_objectID != 142) {
+						/*
+						v6 = v4->getStartColor().a;
+						v13 = 0x3F8000003F800000LL;
+						v14 = 1065353216;
+						v15 = v6;
+						v4->setStartColor(ccc4f(v13, v13, v13, v14));;
+						v12 = v4->getEndColor().a;
+						v13 = 0x3F0000003F000000LL;
+						v15 = 1056964608;
+						v15 = v12;
+						v4->setEndColor(ccc4f(v13, v13, v13, 0));
+						*/
+					}
+				}
+				auto vNumber = cocos2d::CCPoint(0.0, -19.0);
+				this->m_unk31c = vNumber;
+			}
+		}
+	}
+};
+
+class $modify(GameObject)
+{
+    void playDestroyObjectAnim(GJBaseGameLayer *p0)
+    {
+        __int64_t v2;                       // x20
+        __int64_t v4;                       // x23
+        bool v5;                            // w8
+        int v6;                             // w9
+        cocos2d::CCSprite *m_colorSprite;   // x0
+        double *v8;                         // x0
+        double v9;                          // d0 OVERLAPPED
+        GameObjectType m_objectType;        // w24
+        bool hasUniqueCoin;                 // w21
+        double *v12;                        // x0
+        double v13;                         // d0 OVERLAPPED
+        cocos2d::CCParticleSystemQuad *v14; // x0
+        cocos2d::CCParticleSystemQuad *v15; // x22
+        int v16;                            // s0
+        int v22;                            // s0
+        CCCircleWave *v23;                  // x22
+        __int128 __dst;                     // [xsp+60h] [xbp-90h] BYREF
+        __int64_t v130;                     // x0
+        CCCircleWave *v131;                 // x0
+        __int64_t v132;                     // x22
+        int v133;                           // w8
+        __int64_t v134;                     // x0
+        const char *v135;                   // x8
+        GameObjectType v136;                // w9
+        const char *v137;                   // x10
+        const char *v138;                   // x23
+        char *v139;                         // x0
+        char *v140;                         // x20
+        cocos2d::CCNode *v141;              // x21
+        __int64_t v142;                     // x0
+        __int64_t v143;                     // x0
+        __int64_t v144;                     // x0
+        __int64_t v211;                     // [xsp+70h] [xbp-80h]
+        __int64_t v212;                     // [xsp+78h] [xbp-78h] BYREF
+        __int64_t v213;                     // [xsp+80h] [xbp-70h] BYREF
+        float v214;                         // xmm3_4
+        float v215;                         // xmm2_4
+        cocos2d::CCSprite *v216;            // rdx
+        GameObject *v217;                   // rcx
+        int v7;
+
+        if (this->m_objectType == GameObjectType::UserCoin) {
+            hasUniqueCoin = p0->hasUniqueCoin((EffectGameObject *)this);
+            auto v12     = this->getPosition();
+            auto point = CCPoint(v12.x, v12.y);
+            v14           = p0->spawnParticle("coinPickupEffect.plist", 0, kCCPositionTypeGrouped, point);
+            if (this->m_objectType == GameObjectType::Collectible && v14) {
+                /*
+                v16   = v14->getStartColor().a;
+                __dst = 0x3F8000003F800000LL;
+                v6    = 1065353216;
+                v14->setStartColor(ccc4f(__dst, __dst, __dst, v6));
+                v22   = v14->getEndColor().a;
+                __dst = 0x3F0000003F000000LL;
+                v7    = 1056964608;
+                v14->setEndColor(ccc4f(__dst, __dst, __dst, v7));
+                */
+            }
+            v23             = CCCircleWave::create(25.0, 5.0, 0.3, 0, 0);
+            v23->m_color.r  = -14136;
+            v23->m_color.b  = -56;
+            auto vSomething = this->getPosition();
+            v23->setPosition(vSomething);
+            p0->m_objectLayer->addChild(v23, 0);
+            v131             = CCCircleWave::create(5.0, 60.0, 0.3, 0);
+            if (this->m_objectType == GameObjectType::Collectible) {
+                v131->m_color.r  = -14136;
+                v131->m_color.b  = -56;
+            }
+            else {
+                v131->m_color.r  = -14081;
+                v131->m_color.b  = 0;
+            }
+            auto vAnotherOne = this->getPosition();
+            v131->setPosition(vAnotherOne);
+            p0->m_objectLayer->addChild(v131, 0);
+            v131->m_circleMode = CircleMode::Outline;
+            v131->m_lineWidth = 4;
+
+            if (this->m_objectType == GameObjectType::Collectible) {
+                v135 = "secretCoin_2_01_001.png";
+                v137 = "secretCoin_2_b";
+                v138 = "secretCoin_2";
+            }
+            else
+            {
+                v135 = "secretCoin_01_001.png";
+                v137 = "secretCoin";
+                v138 = "secretCoin_b";
+            }
+
+            if (!hasUniqueCoin)
+                v138 = v137;
+
+            auto wow   = cocos2d::CCSprite::createWithSpriteFrameName(v135);
+            auto scale = this->getScaleX();
+            wow->setScaleX(scale);
+            v141 = this->m_pParent;
+            if (v141) {
+                auto defaultOrder = this->m_zOrder;
+                if (!defaultOrder)
+                    defaultOrder = this->m_defaultZOrder;
+                v141->addChild(wow, defaultOrder);
+            }
+            auto again = this->getPosition();
+            wow->setPosition(again);
+            if (this->m_objectType == GameObjectType::Collectible) {
+                auto color_but_with_a_u = this->getColor();
+                wow->setColor(color_but_with_a_u);
+            }
+            auto v145 = cocos2d::CCArray::create();
+            auto v146 = cocos2d::CCString::createWithFormat("%s_01_001.png", v138);
+            auto v147 = v146->getCString();
+            auto v149 = cocos2d::CCSpriteFrameCache::sharedSpriteFrameCache();
+            auto v150 = v149->spriteFrameByName(v147);
+            v145->addObject(v150);
+            auto v151 = cocos2d::CCString::createWithFormat("%s_01_002.png", v138);
+            auto v152 = v151->getCString();
+            auto v154 = cocos2d::CCSpriteFrameCache::sharedSpriteFrameCache();
+            auto v155 = v154->spriteFrameByName(v152);
+            v145->addObject(v155);
+            auto v156 = cocos2d::CCString::createWithFormat("%s_01_003.png", v138);
+            auto v157 = v156->getCString();
+            auto v159 = cocos2d::CCSpriteFrameCache::sharedSpriteFrameCache();
+            auto v160 = v159->spriteFrameByName(v157);
+            v145->addObject(v160);
+            auto v161 = cocos2d::CCString::createWithFormat("%s_01_004.png", v138);
+            auto v162 = v161->getCString();
+            auto v163 = cocos2d::CCSpriteFrameCache::sharedSpriteFrameCache();
+            auto v164 = v163->spriteFrameByName(v162);
+            v145->addObject(v164);
+            auto v165 = cocos2d::CCAnimation::createWithSpriteFrames(v145, 0.1);
+            auto v166 = cocos2d::CCAnimate::create(v165);
+            auto v167 = cocos2d::CCRepeatForever::create(v166);
+            wow->runAction(v167);
+            this->playPickupAnimation(wow, 1.0, 1.0, 0.0, 0.0);
+            return;
+        }
+        else
+            GameObject::playDestroyObjectAnim(p0);
+    }
+};
+
+/*
+// ~patches
+// applies a patch that prevents the game from making the particles in coinEffect.plist silver
+// it's somewhere in EnhancedGameObject::updateUserCoin
+// first one makes it so (if (isCoin && objectID != 142) { ... }) never runs, 142 is secret coin ID
+// second and third changes the coin pickup effect to not be silver for user coins (somewhere in GameObject::playDestroyObjectAnim)
+#ifdef GEODE_IS_WINDOWS
 $execute {
-	// applies a patch that prevents the game from making the particles in coinEffect.plist silver
-	// it's somewhere in EnhancedGameObject::updateUserCoin
-	// first one makes it so (if (isCoin && objectID != 142) { ... }) never runs, 142 is secret coin ID
-	// second and third changes the coin pickup effect to not be silver for user coins 
-	Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x19c9e8), { 0x83 });
-	Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x1a14c1), { 0x74 }); // something 0xc8ffff
-	Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x1a154d), { 0x74 }); // something concat21 0xffff
-	
-	// todo: android support. but that requires arm knowledge
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x19c9e8), { 0x83 });
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x1a14c1), { 0x74 }); // something 0xc8ffff
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x1a154d), { 0x74 }); // something concat21 0xffff
+    }
+ #elif defined(GEODE_IS_ARM_MAC)
+$execute {
+        // applies a patch that prevents the game from making the particles in coinEffect.plist silver
+        // it's somewhere in EnhancedGameObject::updateUserCoin
+        // first one makes it so (if (isCoin && objectID != 142) { ... }) never runs, 142 is secret coin ID
+        // second and third changes the coin pickup effect to not be silver for user coins
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x4f95f8), { 0x83 });
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x2026a0), { 0x74 }); // something 0xc8ffff
+        Mod::get()->patch(reinterpret_cast<void*>(geode::base::get() + 0x202718), { 0x74 }); // something concat21 0xffff
 }
+#endif
+// todo: android support. but that requires arm knowledge
+*/
